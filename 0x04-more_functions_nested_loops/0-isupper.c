4f9ccf8f9c72999e84af099ce53055ee5be9cd56
Siphokazi <usphow@gmail.com>
2023-02-23 14:10:14 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @x: no. to be checked
+ * Return: 1 for uppercase character or 0 for anything else
+ */
+
+int _isupper(int x)
+
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
