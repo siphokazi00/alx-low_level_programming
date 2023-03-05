7eed6407052e680b7d96578f98b5ed11bc98e236
Siphokazi <usphow@gmail.com>
2023-03-05 07:22:49 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
@@ -10,7 +10,7 @@ void more_umbers(void)
 {
 	int x, y;
 
-	for (x =0; x < 10; x++)
+	for (x = 0; x < 10; x++)
 	{
 	for (y = 0; y <= 14; y++)
 	{
@@ -21,4 +21,5 @@ void more_umbers(void)
 	_putchar((y % 10) + '0');
 	}
 	_putchar('\n');
+	}
 }
