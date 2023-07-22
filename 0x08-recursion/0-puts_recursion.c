9b947b84f61276b84045a918f33196e63cf6bfa7
Siphokazi <usphow@gmail.com>
2023-07-22 07:45:15 -0700

    /**

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,3 +10,15 @@ void _puts_recursion(char *s)
 	putchar(*s);
 	_puts_recursion(s + 1);
 }
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
