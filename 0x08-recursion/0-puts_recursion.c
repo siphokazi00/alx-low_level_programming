ba1c3facec3b41e4dd642949f551893063bb95d3
Siphokazi <usphow@gmail.com>
2023-07-22 08:02:46 -0700

    Why is it so important to dream? Because, in my dreams we are together

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - Prints a string, followed by a new line
+ * @s: Pointer to the string to be printed
+ *
+ * Description: This function uses recursion to print each character of the
+ * string.
+ */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
@@ -9,16 +17,4 @@ void _puts_recursion(char *s)
 	}
 	putchar(*s);
 	_puts_recursion(s + 1);
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-	_puts_recursion("Puts with recursion");
-	return (0);
 }
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: Pointer to the string to be printed in reverse
+ *
+ * Description: This function uses recursion to print each character of the
+ * string in reverse order.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}
