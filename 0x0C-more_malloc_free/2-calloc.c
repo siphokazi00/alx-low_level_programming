535d7b5ecb0087ad3c2f13ba97ba6b552b39d853
Siphokazi <usphow@gmail.com>
2023-07-23 10:14:13 -0700

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,6 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int total_size = nmemb * size;
 	void *ptr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -20,8 +21,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (ptr == NULL)
 		return (NULL);
-
-	unsigned int i;
 
 	for (i = 0; i < total_size; i++)
 	{
