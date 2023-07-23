915877f286a955867c7f44a1e6a038a2ecd227b4
Siphokazi <usphow@gmail.com>
2023-07-23 13:38:18 -0700

    SUM

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,25 +1,13 @@
 #ifndef _SUM_
 #define _SUM_
-#include <stdio.h>
 
 /**
- * main - Computes the sum of two numbers x and y.
- * @x: first number
- * @y: second number
+ * SUM - Computes the sum of two numbers x and y.
+ * @x: The first number.
+ * @y: The second number.
  *
  * Return: The sum of x and y.
  */
 #define SUM(x, y) ((x) + (y))
 
-int main(void)
-{
-	int num1 = 5;
-	int num2 = 10;
-
-	int result = SUM(num1, num2);
-
-	printf("Sum of %d and %d is %d\n", num1, num2, result);
-
-	return (0);
-}
 #endif
