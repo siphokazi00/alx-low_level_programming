ec2760a34465fd4468c27d217b62f92f8d396197
Siphokazi <usphow@gmail.com>
2023-07-23 13:14:28 -0700

    Function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,22 @@
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+
+#include <stdio.h>
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+/**
+ * main - Computes the absolute value of a number x.
+ * @x: The number for which to compute the absolute value.
+ *
+ * Return: The absolute value of x.
+ */
+int main(void)
+{
+	int num = -10;
+	int abs_num = ABS(num);
+
+	printf("Absolute value of %d is %d\n", num, abs_num);
+
+	return (0);
+}
+#endif
