9351bdbec7d26b1fc6da36d8cab3be351e7f7842
Siphokazi <usphow@gmail.com>
2023-07-27 14:25:55 -0700

    kancane kancane

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,21 @@
-#ifndef _LISTS_H
-#define _LISTS_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
 #include <stddef.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct list_s
 {
     char *str;
+    int len;
     struct list_s *next;
 } list_t;
 
