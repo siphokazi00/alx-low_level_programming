e6872193c1bc6b91e9934cbfb393d3a21af26e0a
Siphokazi <usphow@gmail.com>
2023-08-03 15:31:50 -0700

    11

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,7 +13,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int mask;
 
 	if (index >= sizeof(unsigned long int) * 8)
-        return (-1);
+		return (-1);
 
 	mask = 1UL << index;
 	*n |= mask;
