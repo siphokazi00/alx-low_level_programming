9bec963086481495a0c51bfa6424848f50bc95d9
Siphokazi <usphow@gmail.com>
2023-08-03 14:26:13 -0700

    2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	mask = 1UL << index;
+	return ((n & mask) ? 1 : 0);
+}
