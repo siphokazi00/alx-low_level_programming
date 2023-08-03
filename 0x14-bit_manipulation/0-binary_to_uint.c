0e98ac1a6edd8b667a7a5c015ef4db2856e62133
Siphokazi <usphow@gmail.com>
2023-08-03 14:14:47 -0700

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		result = (result << 1) | (b[i] - '0');
+	}
+
+	return (result);
+}
