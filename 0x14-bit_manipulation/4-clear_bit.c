80ba0a729e4421311e827753ab39b2506397e5a1
Siphokazi <usphow@gmail.com>
2023-08-03 15:37:25 -0700

    100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,14 +10,9 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask;
-
-	if (index >= sizeof(unsigned long int) * 8)
-	{
+	if (index > 63)
 		return (-1);
-	}
-	mask = ~(1UL << index);
 
-
+	*n = (~(1UL << index) & *n);
 	return (1);
 }
